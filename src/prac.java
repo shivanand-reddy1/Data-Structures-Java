5739f23b54b3ffc453293308b14148bdce8da887
Shiva7785 <shivanandreddy146@gmail.com>
2026-01-29 11:28:13 +0530

    just had practise after long time

--- src/prac.java
+++ src/prac.java
@@ -1,142 +1,21 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
 
-class SLL1{
-    private Node head;
-    private Node tail;
-    private int size;
-
-    private class Node{
-        int val;
-        Node next;
-        Node(int val) {
-            this.val = val;
-        }
-    }
-
-    void print() {
-        Node temp = head;
-        while(temp != null){
-            System.out.print(temp.val + " -> ");
-            temp = temp.next;
-        }
-        System.out.println("Null");
-    }
-
-    void insertAtHead(int val){
-        Node temp = new Node(val);
-        if(head == null) head = tail = temp;
-        else{
-         temp.next = head;
-         head = temp;
-        }
-        size++;
-    }
-
-    void insertAtTail(int val){
-        Node temp = new Node(val);
-        if(head == null) head = tail = temp;
-        else {
-            tail.next = temp;
-            tail = temp;
-        }
-        size++;
-    }
-
-    void insertAtMiddle(int idx, int val){
-
-        if(idx < 0 || idx > size){
-            System.out.println("Invalid Position !!");
-            return;
-        }
-        if(idx == 0){
-            insertAtHead(val);
-            return;
-        }
-        if(idx == size) {
-            insertAtTail(val);
-            return;
-        }
-
-        Node newNode = new Node(val);
-        Node temp = head;
-
-        for(int i=0;i<idx-1;i++){
-            temp = temp.next;
-        }
-        newNode.next = temp.next;
-        temp.next = newNode;
-        size++;
-    }
-
-    void deleteAtBegin(){
-        if(head == null){
-            System.out.println("No nodes are there !!");
-            return;
-        }
-
-        head = head.next;
-        size--;
-    }
-    void deleteAtAny(int idx){
-        if(head == null ){
-            System.out.println("No nodes are there !!");
-            return;
-        }
-        if (head.next == null){
-            head = tail = null;
-        }
-        if(idx < 0 || idx > size -1){
-            System.out.println("Invalid position !!");
-            return;
-        }
-        if(idx == 0){
-            deleteAtBegin();
-        }
-        Node temp = head;
-        for (int i = 0; i < idx-1; i++) {
-            temp = temp.next;
-        }
-        temp.next = temp.next.next;
-        size--;
-    }
-    int getSize() {
-        return size;
-    }
-
-
-}
-
-class Triplet{
-   int dist;
-   int x;
-   int y;
-
-
-   Triplet(int dist, int x, int y){
-       this.dist = dist;
-       this.x = x;
-       this.y = y;
-   }
-}
-
-public class prac {
-
-
-
-    
-
-
+public class prac{
     public static void main(String[] args) {
-        SLL1 ob = new SLL1();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
 
+        System.out.println();
 
-        ob.insertAtHead(10);
-        ob.insertAtHead(20);
-        ob.insertAtHead(30);
-        ob.insertAtHead(40);
-        ob.print();
-
-    }
+        char ch = 'A';
+        for(int i=0;i<n;i++){
+            int num = 1;
+            for(int j=0;j<i+1;j++){
+                System.out.print(num + " ");
+                num+=2;
+            }
+            System.out.println();
+            }
+        }
 }
