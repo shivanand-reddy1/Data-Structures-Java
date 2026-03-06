cf5a6a6c99db387222fd93ac98ed975341f47d68
Shiva7785 <shivanandreddy146@gmail.com>
2026-03-06 14:39:25 +0530

    mazePath

---
+++ src/BackTracking/mazePrintAllPaths.java
@@ -0,0 +1,19 @@
+package BackTracking;
+
+public class mazePrintAllPaths {
+    public static void main(String[] args) {
+        int m = 2, n = 2;
+        printAllPaths(0, 0, m, n, "");
+    }
+
+    private static void printAllPaths(int i, int j, int m, int n, String s) {
+        if(i > m-1 || j > n-1) return;
+        if(i == m-1 && j == n-1){
+            System.out.println(s);
+            return;
+        }
+        printAllPaths(i, j+1, m, n, s+'R');
+        printAllPaths(i+1, j, m, n, s+'D');
+
+    }
+}
